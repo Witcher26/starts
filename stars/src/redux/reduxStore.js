4071cf65048380b1677578b17fa5bfa35171f602
ZvezdilinIL <zvezdilinil@rshbins.ru>
2024-10-21 22:05:40 +0300

    feature-5. redux-thunk

--- stars/src/redux/reduxStore.js
+++ stars/src/redux/reduxStore.js
@@ -1,8 +1,19 @@
  import { createStoreHook } from "react-redux";
 //  import { createStore } from "redux";
  import { combineReducers } from "redux";
+ import {
+    GET_FEED,
+    GET_FEED_FAILED,
+    GET_FEED_SUCCESS
+ } from "../redux/thunks.js"
 
  const GET_GITHUB_DATA_ACTION = "GET_GITHUB_DATA_ACTION";
+
+ const initialState = {
+    feedRequest: false,
+    feedFailed: false,
+    feed: []
+}
 
  const gitHubData = (state = {}, {type, payload}) => {
     switch(type) {
@@ -28,9 +39,48 @@
     }
  }
 
+ const feedReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case GET_FEED: {
+        return {
+          ...state,
+                  // Запрос начал выполняться
+          feedRequest: true,
+                  // Сбрасываем статус наличия ошибок от предыдущего запроса 
+                  // на случай, если он был и завершился с ошибкой
+                  feedFailed: false,
+        };
+      }
+      case GET_FEED_SUCCESS: {
+        return { 
+                  ...state, 
+                  // Запрос выполнился успешно, помещаем полученные данные в хранилище
+                  feed: action.feed, 
+                  // Запрос закончил своё выполнение
+                  feedRequest: false 
+              };
+      }
+      case GET_FEED_FAILED: {
+        return { 
+                  ...state, 
+                  // Запрос выполнился с ошибкой, 
+                  // выставляем соответсвующие значения в хранилище
+                  feedFailed: true, 
+                  // Запрос закончил своё выполнение
+                  feedRequest: false 
+              };
+      }
+          default: {
+              return state
+          }
+      }
+  }
+ 
+
  const rootReducer = combineReducers({
     gitHubData,
-    someReducer
+    someReducer,
+    feed: feedReducer
  });
 
 //  const reduxStore = createStore(rootReducer);
